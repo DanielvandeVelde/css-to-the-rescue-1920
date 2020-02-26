9678b0715ed218ad6628e9aabf2ac55ff28d45af
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-02-27 00:38:25 +0100

    darkmode_without_js

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("keypress", keyPress, false);
-
-function keyPress() {
-  if (document.body.id == "dark") {
-    document.body.id = "light";
-    return;
-  }
-  document.body.id = "dark";
-}
